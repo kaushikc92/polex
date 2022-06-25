821a50683f4ba4787d7256aa8c2b85822b4d66a8
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2022-06-25 10:43:01 +0000

    Upload request code

--- ui/src/FileView.js
+++ ui/src/FileView.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 class FileView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      selectedFile: null
+    };
+    this.getFiles = this.getFiles.bind(this);
+    this.onUploadFileSubmit = this.onUploadFileSubmit.bind(this);
+    this.onUploadFileChange = this.onUploadFileChange.bind(this);
+    this.deleteFile = this.deleteFile.bind(this);
+  }
+  getFiles() {
+    const request = axios({
+      method: "GET",
+      url: `${process.env.PUBLIC_URL}/api/get-files/`
+    });
+    request.then(
+      response => {
+        this.setState({
+          items: response.data.files
+        });
+      }, err => {
+      }
+    );
+  }
+  onUploadFileSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", this.state.selectedFile);
+    formData.append("fileName", this.state.selectedFile.name);
+    const request = axios({
+      method: "POST",
+      url: `${process.env.PUBLIC_URL}/api/upload-file/`,
+      data: formData,
+      headers: {"content-type": "multipart/form-data"}
+    });
+    request.then(
+      response => {
+        this.getFiles();
+      }, err => {
+      }
+    );
+  }
+  onUploadFileChange(e) {
+    this.setState({
+      selectedFile: e.target.files[0]
+    });
+  }
+  deleteFile() {
+  }
   render() {
     return (
-      <div>"File View"</div>
+      <div>
+        <form onSubmit={this.onUploadFileSubmit}>
+          <input type="file" onChange={this.onUploadFileChange} />
+          <button type="submit">{"Upload"}</button>
+        </form>
+      </div>
     );
   }
 }
